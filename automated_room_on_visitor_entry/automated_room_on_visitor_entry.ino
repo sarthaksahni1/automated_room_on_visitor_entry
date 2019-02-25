a380f8d44f1994ece7801b023c094042698fe061
Sarthak Sahni <sarthak138@gmail.com>
2019-02-25 17:21:36 +0530

    Add files via upload

---
+++ automated_room_on_visitor_entry/automated_room_on_visitor_entry.ino
@@ -0,0 +1,44 @@
+#include<LiquidCrystal.h> 
+LiquidCrystal lcd(13,12,11,10,9,8); 
+int In = 4; 
+int Out = 5; 
+int relay = 2; 
+int count=0; 
+void setup() 
+{ 
+  lcd.begin(16,2); 
+  lcd.print("Visitor Counter"); 
+  delay(2000); 
+  pinMode(In, INPUT); 
+  pinMode(Out, INPUT); 
+  pinMode(relay, OUTPUT); 
+  lcd.clear(); 
+  lcd.print("Person In Room:"); 
+  Serial.begin(9600); 
+} 
+void loop() 
+{    
+  if (digitalRead(In) == LOW){ 
+    count++; 
+  } 
+ 
+  else if (digitalRead(Out) == LOW){ 
+    count--; 
+  } 
+   
+  else if (count > 0) 
+  { 
+    digitalWrite(relay, HIGH); 
+  } 
+ 
+  else if(count <= 0){ 
+    digitalWrite(relay, LOW); 
+  } 
+ 
+  lcd.setCursor(0,1); 
+  lcd.print(count); 
+  Serial.println(count);  
+ 
+  delay(700); 
+} 
+
